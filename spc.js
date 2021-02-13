15e86d504ff4a0efe4383c1f474be2d8c1befa1f
toplenboren <toplenboren@yandex.ru>
2021-02-13 15:58:11 +0500

    remove an ability to overwrite package.json from CLI

--- spc.js
+++ spc.js
@@ -1,16 +1,8 @@
 /** A CLI tool to change the pre-commit command from package.json
  * It really has only one function — to set new pre commit hook.
- * If called without any args -> checks the package.json for simple-pre-commit hook and sets the found command as hook
- * Else if called with arg -> sets the hook from arg, also updating the package.json
- * */
-
+ * Checks the package.json for simple-pre-commit hook command and sets the found command as hook
+ */
 const simplePreCommit = require('./simple-pre-commit')
 
-let command = process.argv.slice(2)[0];
-
-if (command === undefined) {
-    command = simplePreCommit.getCommandFromPackageJson()
-} else {
-    simplePreCommit.setCommandInPackageJson(command)
-    simplePreCommit.setPreCommitHook(command)
-}
+const command = simplePreCommit.getCommandFromPackageJson()
+simplePreCommit.setPreCommitHook(command)

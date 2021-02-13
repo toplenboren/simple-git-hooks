5d70e9bd1e195dc02b0df39c84674909f6652a1c
toplenboren <toplenboren@yandex.ru>
2021-02-13 17:21:50 +0500

    fix execution of postinstall script

--- postinstall.js
+++ postinstall.js
@@ -1,15 +1,20 @@
-const {setPreCommitHook, packageInDevDependencies, getPackageJson} = require("./simple-pre-commit");
+const {getCommandFromPackageJson, getPackageJson, packageInDevDependencies, setPreCommitHook} = require("./simple-pre-commit");
 
 /**
  * Post-installs the script
  * 1. Creates the pre-commit hook with npx lint-staged command by default
  */
 function postinstall() {
-    const {packageJsonContent} = getPackageJson()
+    const { packageJsonContent } = getPackageJson()
 
     if (packageInDevDependencies(packageJsonContent)) {
         try {
-            setPreCommitHook("npx lint staged")
+            const command = getCommandFromPackageJson()
+            if (command === undefined) {
+                console.log('[INFO] Please add the pre-commit command to the "simple-pre-commit" field in package.json')
+            } else {
+                setPreCommitHook("npx lint staged")
+            }
         } catch (err) {
             console.log('[ERROR] Was not able to create a pre-commit hook. Reason: ' + err)
         }

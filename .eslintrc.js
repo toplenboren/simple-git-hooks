eef8b8d31fc09fde933615b17338cb2aff540a57
toplenboren <toplenboren@yandex.ru>
2021-02-14 17:49:12 +0500

    add jest environment to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "commonjs": true,
         "es2021": true,
-        "node": true
+        "node": true,
+        "jest": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
